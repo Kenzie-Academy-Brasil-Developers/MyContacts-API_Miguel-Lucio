618f11761641e7234de6c17dd21ae011be888b02
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-01-26 18:00:15 -0300

    feat: create entities

---
+++ src/entities/contact.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { User } from "./user.entity";
+
+@Entity("contacts")
+export class Contact {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ length: 120 })
+  fullName: string;
+
+  @Column({ length: 50, unique: true })
+  email: string;
+
+  @Column({ length: 20 })
+  phone: string;
+
+  @CreateDateColumn({ type: 'date' })
+  registeredAt: string;
+
+  @ManyToOne(() => User, (u) => u.contacts)
+  user: User;
+}
---
+++ src/entities/user.entity.ts
@@ -0,0 +1,44 @@
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { getRounds, hashSync } from "bcryptjs";
+import { Contact } from "./contact.entity";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ length: 120 })
+  fullName: string;
+
+  @Column({ length: 50, unique: true })
+  email: string;
+
+  @Column({ length: 120 })
+  password: string;
+
+  @Column({ length: 20 })
+  phone: string;
+
+  @CreateDateColumn({ type: "date" })
+  registeredAt: string;
+
+  @OneToMany(() => Contact, (c) => c.user)
+  contacts: Array<Contact>;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    const hasRounds: number = getRounds(this.password);
+    if (!hasRounds) {
+      this.password = hashSync(this.password, 10);
+    }
+  }
+}
