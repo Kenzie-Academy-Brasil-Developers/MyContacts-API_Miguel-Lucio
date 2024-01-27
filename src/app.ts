3c2bfacaa428251c749aaf9f165b66b2fcb17df8
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-01-26 23:05:58 -0300

    feat: implementing routes and handleAppError

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import "reflect-metadata";
 import "express-async-errors";
 import express, { Application, json } from "express";
+import { handleAppError } from "./middlewares/handleAppError.middleware";
+import { routes } from "./routes";
 
 const app: Application = express();
 
 app.use(json());
 
+app.use("/", routes);
+
+app.use(handleAppError);
 export default app;
