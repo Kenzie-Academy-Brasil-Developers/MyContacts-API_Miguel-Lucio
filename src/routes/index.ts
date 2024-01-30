fc4db103a91c3919d11ec5d10b5a036ce666db38
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-01-30 20:37:43 -0300

    feat: implementing contactRouter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userRouter } from "./user.route";
 import { loginRouter } from "./login.route";
+import { contactRouter } from "./contact.route";
 
 export const routes: Router = Router();
 
 routes.use("/users", userRouter);
 routes.use("/login", loginRouter);
+routes.use("/contacts", contactRouter);
