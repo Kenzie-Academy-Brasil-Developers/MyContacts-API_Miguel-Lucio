5d7e445f9505c1a2e5ef0991f617a0495024b8ac
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-01-30 20:31:57 -0300

    feat: contact repository

--- src/repositories.ts
+++ src/repositories.ts
@@ -1,7 +1,10 @@
 import { Repository } from "typeorm";
 import { User } from "./entities/user.entity";
+import { Contact } from "./entities/contact.entity";
 import { AppDataSource } from "./data-source";
 
 const userRepository: Repository<User> = AppDataSource.getRepository(User);
+const contactRepository: Repository<Contact> =
+  AppDataSource.getRepository(Contact);
 
-export { userRepository };
+export { userRepository, contactRepository };
