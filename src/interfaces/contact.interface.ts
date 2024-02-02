ddc12bc9e46797389d52d13d890b52d64e16690b
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-02-02 16:41:11 -0300

    feat: type TContact

--- src/interfaces/contact.interface.ts
+++ src/interfaces/contact.interface.ts
@@ -2,11 +2,13 @@ import { z } from "zod";
 import {
   contactCreateSchema,
   contactListSchema,
+  contactSchema,
 } from "../schemas/contact.schema";
 import { DeepPartial } from "typeorm";
 
+type TContact = z.infer<typeof contactSchema>;
 type TContactCreate = z.infer<typeof contactCreateSchema>;
 type TContactUpdate = DeepPartial<TContactCreate>;
 type TContactList = z.infer<typeof contactListSchema>;
 
-export { TContactCreate, TContactUpdate, TContactList };
+export { TContact, TContactCreate, TContactUpdate, TContactList };
