7a0fb0efb4cd56e8a9716f985ab06dc3c9ad9678
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-02-04 13:17:29 -0300

    feat: verify uuid format

--- src/middlewares/verifyId.middleware.ts
+++ src/middlewares/verifyId.middleware.ts
@@ -8,11 +8,17 @@ export const verifyId = async (
   next: NextFunction
 ) => {
   const { id } = req.params;
+  const uuidRegex =
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
 
-  const foundUser = await userRepository.findOneBy({ id });
-  if (!foundUser) throw new AppError("User not found.", 404);
+  if (uuidRegex.test(id)) {
+    const foundUser = await userRepository.findOneBy({ id });
+    if (!foundUser) throw new AppError("User not found.", 404);
 
-  res.locals = { ...res.locals, foundUser };
+    res.locals = { ...res.locals, foundUser };
+  } else {
+    throw new AppError("User not found.", 404);
+  }
 
   return next();
 };
