556d844d30d7689bbe781220f29a0bf9e1b3714a
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-02-04 14:44:23 -0300

    feat: unistall swagger

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import "reflect-metadata";
 import "express-async-errors";
-import swaggerUiExpress from "swagger-ui-express";
-import swaggerDocument from "./swagger.json";
 import express, { Application, json } from "express";
 import { handleAppError } from "./middlewares/handleAppError.middleware";
 import { routes } from "./routes";
@@ -10,11 +8,6 @@ const app: Application = express();
 
 app.use(json());
 
-app.use(
-  "/api-documentation",
-  swaggerUiExpress.serve,
-  swaggerUiExpress.setup(swaggerDocument)
-);
 app.use("/", routes);
 
 app.use(handleAppError);
