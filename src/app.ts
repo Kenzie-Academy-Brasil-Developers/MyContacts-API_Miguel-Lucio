28c98db1871b0c5073dd963ffbc6d5e8d94773ff
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2024-02-10 15:42:38 -0300

    feat: cors implemented

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,16 @@ import "express-async-errors";
 import express, { Application, json } from "express";
 import { handleAppError } from "./middlewares/handleAppError.middleware";
 import { routes } from "./routes";
+import cors from "cors";
 
 const app: Application = express();
 
 app.use(json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use("/", routes);
 
